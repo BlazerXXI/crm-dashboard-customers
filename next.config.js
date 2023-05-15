c7d351dc8231812323629a8af0852ce7c75b8b6b
BlazerXXI <ruslandendik@gmail.com>
2023-05-15 22:24:10 +0300

    dev

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
-	basePath: '/crm-dashboard-customers',
-	assetPrefix: '/crm-dashboard-customers',
-	output: 'export',
+  basePath: "/crm-dashboard-customers",
+  assetPrefix: "/crm-dashboard-customers",
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
